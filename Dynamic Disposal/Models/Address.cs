ee68fce91ab4d30d386018972ec706069667c31f
JakeDrohman <jkdrohman@gmail.com>
2016-10-24 21:04:41 -0500

    added address and PickUpDays classes

---
+++ Dynamic Disposal/Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Dynamic_Disposal.Models
+{
+    public class Address
+    {
+        [Key]
+        public int AddressID { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        [ForeignKey("User")]
+        public string UserID { get; set; }
+        public ApplicationUser User { get; set; }
+        [ForeignKey("PickUpDay")]
+        public int PickUpDayID { get; set; }
+        public PickUpDay PickUpDay { get; set; }
+    }
+}
---
+++ Dynamic Disposal/Models/PickUpDay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dynamic_Disposal.Models
+{
+    public class PickUpDay
+    {
+        [Key]
+        public int DayID { get; set; }
+        public string DayValue { get; set; }
+    }
+}

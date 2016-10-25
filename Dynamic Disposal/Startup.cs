513967647ade4d86fc69817b0e2225212f56c08d
JakeDrohman <jkdrohman@gmail.com>
2016-10-24 20:40:10 -0500

    Add project files.

---
+++ Dynamic Disposal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dynamic_Disposal.Startup))]
+namespace Dynamic_Disposal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
